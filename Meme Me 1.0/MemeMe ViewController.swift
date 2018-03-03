a665bdae2dcbe60ab66747897f02980decf78ff6
akhilmantha <akhilmantha@live.com>
2018-03-03 14:23:29 +0530

    initial commit

---
+++ Meme Me 1.0/MemeMe ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MemeMe ViewController.swift
+//  Meme Me 1.0
+//
+//  Created by akhil mantha on 03/03/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import UIKit
+
+class MemeMe_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
