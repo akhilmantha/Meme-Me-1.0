29d7b16709b76578e227bc7ebdc3acc0ca434167
akhilmantha <akhilmantha@live.com>
2018-04-02 02:23:57 +0530

    refactoring 1.0

---
+++ Meme Me 1.0/MemeMeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeMeCollectionViewCell.swift
+//  Meme Me 1.0
+//
+//  Created by akhil mantha on 01/04/18.
+//  Copyright © 2018 akhil mantha. All rights reserved.
+//
+
+import UIKit
+
+class MemeMeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memedImageView : UIImageView!
+}
--- Meme Me 1.0/MemeMeDetailViewController.swift
+++ Meme Me 1.0/MemeMeDetailViewController.swift
@@ -9,27 +9,18 @@
 import UIKit
 
 class MemeMeDetailViewController: UIViewController {
-
+    
+    
+    var selectedMeme : Meme!
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        detailImageView.image = selectedMeme.memedImage
+        detailImageView.contentMode = .scaleAspectFit
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
